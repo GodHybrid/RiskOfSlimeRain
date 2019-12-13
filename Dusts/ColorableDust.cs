85a3c10d992268360e4cded4a7edc0409d02ba3a
direwolf420 <amakejk1@web.de>
2019-12-13 15:15:16 +0100

    format dust

--- Dusts/ColorableDust.cs
+++ Dusts/ColorableDust.cs
@@ -7,32 +7,32 @@ namespace RiskOfSlimeRain.Dusts
 	/// <summary>
 	/// Flies in a straight line, colorable, doesn't glow. Can produce light with the same color as the dust
 	/// </summary>
-    public class ColorableDust : ModDust
-    {
-        public override void OnSpawn(Dust dust)
-        {
-            dust.noGravity = true;
-            dust.noLight = true;
-        }
+	public class ColorableDust : ModDust
+	{
+		public override void OnSpawn(Dust dust)
+		{
+			dust.noGravity = true;
+			dust.noLight = true;
+		}
 
-        public override bool Update(Dust dust)
-        {
-            dust.position += dust.velocity;
-            dust.scale *= 0.99f;
-            if (dust.scale < 0.5f)
-            {
-                dust.active = false;
-            }
+		public override bool Update(Dust dust)
+		{
+			dust.position += dust.velocity;
+			dust.scale *= 0.99f;
+			if (dust.scale < 0.5f)
+			{
+				dust.active = false;
+			}
 			if (!dust.noLight)
 			{
 				Lighting.AddLight(dust.position, ((Color)GetAlpha(dust, dust.color * 0.5f)).ToVector3());
 			}
-            return false;
-        }
+			return false;
+		}
 
-        public override Color? GetAlpha(Dust dust, Color lightColor)
-        {
-            return lightColor * ((255 - dust.alpha) / 255f);
-        }
-    }
+		public override Color? GetAlpha(Dust dust, Color lightColor)
+		{
+			return lightColor * ((255 - dust.alpha) / 255f);
+		}
+	}
 }
