a30baa56f6ee5237045aabbdaa7f6413af896db8
direwolf420 <amakejk1@web.de>
2020-01-20 22:42:15 +0100

    fix description on fire shield

--- Effects/Common/FireShieldEffect.cs
+++ Effects/Common/FireShieldEffect.cs
@@ -10,11 +10,11 @@ namespace RiskOfSlimeRain.Effects.Common
 {
 	public class FireShieldEffect : RORCommonEffect, IPostHurt
 	{
-		const int dmg = 200;
+		const float dmg = 2f;
 		const float kb = 20;
 		const float hplimit = 0.1f;
 
-		public override string Description => $"After being hit for {hplimit.ToPercent()} of your max health - explode, dealing {dmg} damage";
+		public override string Description => $"After being hit for {hplimit.ToPercent()} of your max health - explode, dealing {dmg.ToPercent()} damage";
 
 		public override string FlavorText => "The thing is only half-done, but it will do the job\nPLEASE handle with care!";
 
@@ -22,7 +22,7 @@ namespace RiskOfSlimeRain.Effects.Common
 		{
 			if (damage >= player.statLifeMax2 * hplimit)
 			{
-				Projectile.NewProjectile(player.position, Vector2.Zero, ModContent.ProjectileType<FireShieldExplosion>(), Math.Max(20, 2 * Stack * player.GetDamage()), kb + Stack, Main.myPlayer);
+				Projectile.NewProjectile(player.position, Vector2.Zero, ModContent.ProjectileType<FireShieldExplosion>(), (int)Math.Max(20, dmg * Stack * player.GetDamage()), kb + Stack, Main.myPlayer);
 			}
 		}
 	}
