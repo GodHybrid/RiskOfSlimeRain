27d2218a59a3751586725e7305f3803354a01dfe
direwolf420 <amakejk1@web.de>
2020-02-24 12:50:22 +0100

    fix double crit on harvesters scythe

--- Core/ROREffects/Uncommon/HarvestersScytheEffect.cs
+++ Core/ROREffects/Uncommon/HarvestersScytheEffect.cs
@@ -4,7 +4,7 @@ using Terraria;
 
 namespace RiskOfSlimeRain.Core.ROREffects.Uncommon
 {
-	public class HarvestersScytheEffect : RORUncommonEffect, IModifyHit, IOnHit, IGetWeaponCrit
+	public class HarvestersScytheEffect : RORUncommonEffect, IOnHit, IGetWeaponCrit
 	{
 		public const float initial = 6;
 		public const float increase = 2;
@@ -12,25 +12,12 @@ namespace RiskOfSlimeRain.Core.ROREffects.Uncommon
 
 		public float CurrentHeal => initial + increase * Stack;
 
+		//TODO
 		public float StoredHeals = 0;
 
-		//public override bool AlwaysProc => true;
 		public override string Description => $"Gain 5% crit chance. Critical strikes heal for {initial + increase} HP";
 		public override string FlavorText => "It takes a brave man to look death in the eye and claim they don't need help.";
 		public override string Name => "Harvester's Scythe";
-
-
-		public void ModifyHitNPC(Player player, Item item, NPC target, ref int damage, ref float knockback, ref bool crit)
-		{
-			if (!Proc(critChance)) return;
-			crit = true;
-		}
-
-		public void ModifyHitNPCWithProj(Player player, Projectile proj, NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
-		{
-			if (!Proc(critChance)) return;
-			crit = true;
-		}
 
 		public void OnHitNPC(Player player, Item item, NPC target, int damage, float knockback, bool crit)
 		{
