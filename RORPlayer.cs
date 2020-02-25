139d11e0b3502b4698415213c0af7d5b58cbdf04
direwolf420 <amakejk1@web.de>
2020-02-25 18:34:28 +0100

    fix ordering of some onhit stuff

--- RORPlayer.cs
+++ RORPlayer.cs
@@ -166,16 +166,16 @@ namespace RiskOfSlimeRain
 			//This stuff should be at the bottom of everything
 			if (!IsHostile(target)) return;
 
-			if (target.life <= 0)
-			{
-				ROREffectManager.Perform<IOnKill>(this, e => e.OnKillNPCWithProj(player, proj, target, damage, knockback, crit));
-			}
+			//If this projectile shouldn't proc at all
+			if (proj.modProjectile is IExcludeOnHit) return;
 
 			//If this projectile is a minion, make it only proc 10% of the time
 			if ((proj.minion || ProjectileID.Sets.MinionShot[proj.type]) && !Main.rand.NextBool(10)) return;
 
-			//If this projectile shouldn't proc at all
-			if (proj.modProjectile is IExcludeOnHit) return;
+			if (target.life <= 0)
+			{
+				ROREffectManager.Perform<IOnKill>(this, e => e.OnKillNPCWithProj(player, proj, target, damage, knockback, crit));
+			}
 
 			ROREffectManager.Perform<IOnHit>(this, e => e.OnHitNPCWithProj(player, proj, target, damage, knockback, crit));
 		}
@@ -187,10 +187,10 @@ namespace RiskOfSlimeRain
 
 			//If this projectile shouldn't proc at all
 			if (proj.modProjectile is IExcludeOnHit) return;
+
 			//If this projectile is a minion, make it only proc 10% of the time
 			if ((proj.minion || ProjectileID.Sets.MinionShot[proj.type]) && !Main.rand.NextBool(10)) return;
 
-			Main.NewText("proc");
 			ROREffectManager.ModifyHitNPCWithProj(player, proj, target, ref damage, ref knockback, ref crit, ref hitDirection);
 		}
 
