72b7d0034ac585fa7d633e6d450d9bf00d0b18ed
direwolf420 <amakejk1@web.de>
2020-03-04 19:51:45 +0100

    add bitter root visuals

--- Core/ROREffects/Common/BitterRootEffect.cs
+++ Core/ROREffects/Common/BitterRootEffect.cs
@@ -1,14 +1,37 @@
 ﻿using RiskOfSlimeRain.Core.ROREffects.Interfaces;
+using RiskOfSlimeRain.Dusts;
 using RiskOfSlimeRain.Helpers;
 using Terraria;
 
 namespace RiskOfSlimeRain.Core.ROREffects.Common
 {
-	public class BitterRootEffect : RORCommonEffect, IResetEffects
+	public class BitterRootEffect : RORCommonEffect, IResetEffects, IPostUpdateEquips
 	{
 		const float increase = 0.07895f;
 
+		public const int initialSpawnTimerMin = 40;
+
+		public const int initialSpawnTimerMax = 100;
+
+		public const int fastSpawnTimerMin = 4;
+
+		public const int fastSpawnTimerMax = 10;
+
+		public const int fastSpawnRate = 3;
+
+		public int initialSpawnTimer = 0;
+
+		public int initialSpawnTimerNext = initialSpawnTimerMax;
+
+		public int fastSpawnTimer = 0;
+
+		public int fastSpawnTimerNext = fastSpawnTimerMax;
+
+		public bool spawning = false;
+
 		public override int MaxRecommendedStack => 38;
+
+		public override bool EnforceMaxStack => true;
 
 		public override string Description => $"Permanently increases maximum life by roughly {increase.ToPercent(0)}";
 
@@ -24,6 +47,36 @@ namespace RiskOfSlimeRain.Core.ROREffects.Common
 		public int GetIncreaseAmount(Player player)
 		{
 			return (int)(player.statLifeMax * Stack * increase);
+		}
+
+		public void PostUpdateEquips(Player player)
+		{
+			if (Config.HiddenVisuals(player)) return;
+
+			if (!spawning)
+			{
+				initialSpawnTimer++;
+				if (initialSpawnTimer >= initialSpawnTimerNext)
+				{
+					initialSpawnTimer = 0;
+					spawning = true;
+					initialSpawnTimerNext = Main.rand.Next(initialSpawnTimerMin, initialSpawnTimerMax);
+				}
+			}
+			if (spawning)
+			{
+				fastSpawnTimer++;
+				if (fastSpawnTimer % fastSpawnRate == 0)
+				{
+					Dust dust = BitterRootDust.NewDust(player);
+				}
+				if (fastSpawnTimer >= fastSpawnTimerNext)
+				{
+					fastSpawnTimer = 0;
+					fastSpawnTimerNext = fastSpawnRate * Main.rand.Next(fastSpawnTimerMin, fastSpawnTimerMax);
+					spawning = false;
+				}
+			}
 		}
 	}
 }
---
+++ Dusts/BitterRootDust.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace RiskOfSlimeRain.Dusts
+{
+	public class BitterRootData
+	{
+		public const int frameCount = 4;
+		public const int frameSpeed = 5;
+		public const int height = 20;
+		public const int width = 10;
+
+		public int AlphaReduceRate { get; private set; }
+
+		public int Frame { get; set; }
+
+		public int FrameCounter { get; set; }
+
+		public Dust Dust { get; private set; }
+
+		public BitterRootData(Dust dust, int alphaReduceRate = 5, int frame = -1)
+		{
+			Dust = dust;
+			AlphaReduceRate = alphaReduceRate;
+			FrameCounter = 0;
+			Frame = frame == -1 ? Main.rand.Next(frameCount) : frame;
+		}
+
+		public void Animate()
+		{
+			FrameCounter++;
+			if (FrameCounter >= frameSpeed)
+			{
+				FrameCounter = 0;
+				Frame++;
+				if (Frame >= frameCount)
+				{
+					Frame = 0;
+				}
+			}
+
+			Dust.frame = new Rectangle(0, Frame * height, width, height);
+		}
+	}
+
+	/// <summary>
+	/// Use custom NewDust method to spawn
+	/// </summary>
+	public class BitterRootDust : ModDust
+	{
+		public static Dust NewDust(Player player, int alphaReduceRate = 5)
+		{
+			Dust dust = Dust.NewDustDirect(player.Center, 0, 0, ModContent.DustType<BitterRootDust>(), Alpha: 100, newColor: Color.White);
+			dust.velocity = new Vector2(-1, 0).RotatedBy(Main.rand.NextDouble() * MathHelper.Pi); //Only upwards, rotation clock wise
+			dust.velocity *= 0.3f + Main.rand.NextFloat(1f);
+			dust.position += 16 * dust.velocity;
+			dust.position.Y -= 10;
+			dust.rotation = Main.rand.NextFloat(MathHelper.TwoPi);
+			dust.customData = new BitterRootData(dust, alphaReduceRate, -1);
+			BitterRootData data = (BitterRootData)dust.customData;
+			data.Animate();
+			return dust;
+		}
+
+		private BitterRootData GetData(Dust dust)
+		{
+			BitterRootData data = null;
+			if (dust.customData is BitterRootData) data = (BitterRootData)dust.customData;
+			else dust.customData = new BitterRootData(dust);
+			return data;
+		}
+
+		public override void OnSpawn(Dust dust)
+		{
+			dust.noGravity = true;
+			dust.noLight = true;
+		}
+
+		public override bool Update(Dust dust)
+		{
+			BitterRootData data = GetData(dust);
+			dust.position += dust.velocity;
+			data.Animate();
+
+			dust.alpha += data.AlphaReduceRate;
+			if (dust.alpha > 255)
+			{
+				dust.active = false;
+			}
+			return false;
+		}
+
+		public override Color? GetAlpha(Dust dust, Color lightColor)
+		{
+			return dust.color.MultiplyRGBA(lightColor) * ((255 - dust.alpha) / 255f);
+		}
+	}
+}
