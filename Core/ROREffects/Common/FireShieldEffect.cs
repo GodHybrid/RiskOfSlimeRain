0ed45ceeefaf465dc524cf114a8862313667dbfb
direwolf420 <amakejk1@web.de>
2020-05-04 15:35:07 +0200

    balance fire shield

--- Core/ROREffects/Common/FireShieldEffect.cs
+++ Core/ROREffects/Common/FireShieldEffect.cs
@@ -11,23 +11,25 @@ namespace RiskOfSlimeRain.Core.ROREffects.Common
 	public class FireShieldEffect : RORCommonEffect, IPostHurt
 	{
 		const float dmg = 2f;
-		const float kb = 20;
-		const float hplimit = 0.1f;
 
-		public override string Description => $"After being hit for {hplimit.ToPercent()} of your max health - explode, dealing {dmg.ToPercent()} damage";
+		private float KB => ServerConfig.Instance.RorStats ? 20 : 10;
+
+		private float HPlimit => ServerConfig.Instance.RorStats ? 0.1f : 0.2f;
+
+		public override string Description => $"After being hit for {HPlimit.ToPercent()} of your max health - explode, dealing {dmg.ToPercent()} damage";
 
 		public override string FlavorText => "The thing is only half-done, but it will do the job\nPLEASE handle with care!";
 
 		public override string UIInfo()
 		{
-			return $"Damage: {dmg.ToPercent()}, Knockback: {kb.ToPercent()}";
+			return $"Damage: {dmg.ToPercent()}, Knockback: {KB.ToPercent()}";
 		}
 
 		public void PostHurt(Player player, bool pvp, bool quiet, double damage, int hitDirection, bool crit)
 		{
-			if (damage >= player.statLifeMax2 * hplimit)
+			if (damage >= player.statLifeMax2 * HPlimit)
 			{
-				Projectile.NewProjectile(player.position, Vector2.Zero, ModContent.ProjectileType<FireShieldExplosion>(), (int)Math.Max(20, dmg * Stack * player.GetDamage()), kb + Stack, Main.myPlayer);
+				Projectile.NewProjectile(player.position, Vector2.Zero, ModContent.ProjectileType<FireShieldExplosion>(), (int)Math.Max(20, dmg * Stack * player.GetDamage()), KB + Stack, Main.myPlayer);
 			}
 		}
 	}
