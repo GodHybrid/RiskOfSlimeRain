6d8fba0123894ac6120a2baf41e7f0fa911bc48e
direwolf420 <amakejk1@web.de>
2020-05-13 19:12:52 +0200

    add background

---
+++ Backgrounds/FirstLevelBackground.cs
@@ -0,0 +1,43 @@
+using RiskOfSlimeRain.Core.Subworlds;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace RiskOfSlimeRain.Backgrounds
+{
+	public class FirstLevelBackground : ModSurfaceBgStyle
+	{
+		public override bool ChooseBgStyle()
+		{
+			return !Main.gameMenu && (SubworldManager.IsActive(FirstLevelBasic.id) ?? false);
+		}
+
+		// Use this to keep far Backgrounds like the mountains
+		public override void ModifyFarFades(float[] fades, float transitionSpeed)
+		{
+			for (int i = 0; i < fades.Length; i++)
+			{
+				if (i == Slot)
+				{
+					fades[i] += transitionSpeed;
+					if (fades[i] > 1f)
+					{
+						fades[i] = 1f;
+					}
+				}
+				else
+				{
+					fades[i] -= transitionSpeed;
+					if (fades[i] < 0f)
+					{
+						fades[i] = 0f;
+					}
+				}
+			}
+		}
+
+		public override int ChooseMiddleTexture()
+		{
+			return mod.GetBackgroundSlot("Backgrounds/FirstLevelBackgroundMid");
+		}
+	}
+}
