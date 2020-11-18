fa34f6caf7476bc1fae2c7878a09dd655d5b6ce0
direwolf420 <amakejk1@web.de>
2020-11-18 15:30:10 +0100

    rebalance tough times

--- Core/ROREffects/Uncommon/ToughTimesEffect.cs
+++ Core/ROREffects/Uncommon/ToughTimesEffect.cs
@@ -1,25 +1,30 @@
 ﻿using RiskOfSlimeRain.Core.ROREffects.Interfaces;
 using RiskOfSlimeRain.Helpers;
+using System;
 using Terraria;
 
 namespace RiskOfSlimeRain.Core.ROREffects.Uncommon
 {
-	//TODO needs rebalancing badly
 	public class ToughTimesEffect : RORUncommonEffect, IPostUpdateEquips
 	{
-		public override float Initial => 14;
+		public override float Initial => 0.07f;
 
-		public override float Increase => 7;
+		public override float Increase => 0.07f;
 
-		public float DefInc => Formula();
+		public float DRIncrease => 1f - (float)Math.Pow(1f - Initial, Math.Max(1, Stack));
 
-		public override string Description => $"Increase armor by {Initial}";
+		public override string Description => $"Increase damage reduction by {Initial.ToPercent()}, stacks multiplicatively";
 
 		public override string FlavorText => "Bears are just about the only toy that can lose just about everything and still maintain their dignity and worth.\nDon't forget, hon. I'm coming home soon. Stay strong.";
 
+		public override string UIInfo()
+		{
+			return $"Additional DR: {DRIncrease.ToPercent(2)}";
+		}
+
 		public void PostUpdateEquips(Player player)
 		{
-			player.statDefense += (int)DefInc;
+			player.endurance += DRIncrease;
 		}
 	}
 }
