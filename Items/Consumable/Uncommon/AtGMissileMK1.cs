8abce6efcea5eaa4a5f8da2203498ddbff7aa59d
direwolf420 <amakejk1@web.de>
2020-11-19 13:48:59 +0100

    remove uncommon recipes

--- Items/Consumable/Uncommon/AtGMissileMK1.cs
+++ Items/Consumable/Uncommon/AtGMissileMK1.cs
@@ -1,18 +1,9 @@
 ﻿using RiskOfSlimeRain.Core.ROREffects.Uncommon;
-using Terraria.ID;
-using Terraria.ModLoader;
 
 namespace RiskOfSlimeRain.Items.Consumable.Uncommon
 {
 	public class AtGMissileMK1 : RORConsumableItem<AtGMissileMK1Effect>
 	{
-		public override void AddRecipes()
-		{
-			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.DirtBlock, 60);
 
-			recipe.SetResult(this, 1);
-			recipe.AddRecipe();
-		}
 	}
 }
