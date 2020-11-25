0bb69c620af9549cd9912389e2f22ff81898a4eb
direwolf420 <amakejk1@web.de>
2020-11-25 22:25:12 +0100

    magma worm fireballs don't make a sound and only bounce x6

--- Projectiles/Hostile/FireBallGravityBouncy.cs
+++ Projectiles/Hostile/FireBallGravityBouncy.cs
@@ -1,11 +1,12 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace RiskOfSlimeRain.Projectiles.Hostile
 {
 	/// <summary>
-	/// Increased gravity in expert mode by 50%
+	/// Increased gravity in expert mode
 	/// </summary>
 	public class FireballGravityBouncy : ModProjectile
 	{
@@ -25,13 +26,49 @@ namespace RiskOfSlimeRain.Projectiles.Hostile
 		public override void SetDefaults()
 		{
 			projectile.CloneDefaults(ProjectileID.Fireball);
+			projectile.aiStyle = -1;
 			projectile.timeLeft = 540;
-			projectile.penetrate = -1;
+			projectile.penetrate = 6;
+		}
+
+		public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+			projectile.penetrate--;
+			if (projectile.penetrate <= 0)
+			{
+				projectile.Kill();
+				return false;
+			}
+			if (projectile.velocity.X != oldVelocity.X)
+			{
+				projectile.velocity.X = -oldVelocity.X;
+			}
+			if (projectile.velocity.Y != oldVelocity.Y)
+			{
+				projectile.velocity.Y = -oldVelocity.Y;
+			}
+			return false;
+		}
+
+		public override Color? GetAlpha(Color lightColor)
+		{
+			return Color.White;
 		}
 
 		public override void AI()
 		{
-			projectile.velocity.Y += Main.expertMode ? 0.015f : 0.01f;
+			projectile.velocity.Y += Main.expertMode ? 0.18f : 0.165f;
+			projectile.rotation += 0.8f;
+
+			for (int i = 0; i < 2; i++)
+			{
+				if (Main.rand.NextFloat() < 0.2f) continue;
+				Dust dust = Dust.NewDustDirect(projectile.position, projectile.width, projectile.height, DustID.Fire, 0f, 0f, 100);
+				dust.scale += 0.4f + Main.rand.NextFloat(0.5f);
+				dust.noGravity = true;
+				dust.velocity *= 0.2f;
+				dust.noLight = true;
+			}
 		}
 	}
 }
